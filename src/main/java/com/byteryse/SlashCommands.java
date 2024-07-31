3179a0e3dec1b878043f6e53d5eeaf97750a2d68
Ben Walker <benjaminjwalker2910@gmail.com>
2024-07-31 10:22:14 +0100

    campaign creation refactor

--- src/main/java/com/byteryse/SlashCommands.java
+++ src/main/java/com/byteryse/SlashCommands.java
@@ -8,17 +8,13 @@ import com.byteryse.DTO.Campaign;
 import com.byteryse.Database.DatabaseController;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.channel.concrete.Category;
 import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
-import net.dv8tion.jda.api.entities.channel.forums.ForumPost;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -31,17 +27,14 @@ import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
-import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 
 public class SlashCommands extends ListenerAdapter {
-	private DatabaseController dbCon;
 	private CampaignDAO campaignDAO;
 
 	private static final String CAMPAIGN_FORUM = "1263135869645094993";
 	private static final String GAME_ANNOUNCEMENTS = "1252710143838261399";
 
 	public SlashCommands(DatabaseController dbCon) {
-		this.dbCon = dbCon;
 		this.campaignDAO = new CampaignDAO(dbCon);
 	}
 
@@ -50,11 +43,8 @@ public class SlashCommands extends ListenerAdapter {
 		String command = event.getName();
 
 		switch (command) {
-			case "ping":
-				event.reply("PONG").queue();
-				break;
 			case "create-campaign":
-				createCampaign(event);
+				CampaignManagement.campaignCreationModal(event);
 				break;
 			default:
 				event.reply("Something went wrong.");
@@ -62,134 +52,10 @@ public class SlashCommands extends ListenerAdapter {
 		}
 	}
 
-	private void createCampaign(SlashCommandInteractionEvent event) {
-		if (!event.getMember().getRoles().contains(event.getGuild().getRoleById("1252378924961370182"))) {
-			event.reply(
-					"You must be a Game Master to create campaigns here. Consider applying to become one if you're interested!")
-					.setEphemeral(true).queue();
-			return;
-		}
-
-		TextInput name = TextInput.create("name", "Name", TextInputStyle.SHORT)
-				.setPlaceholder("The name of your campaign")
-				.setMinLength(3)
-				.setMaxLength(25)
-				.setRequired(true)
-				.build();
-
-		TextInput description = TextInput.create("description", "Description", TextInputStyle.PARAGRAPH)
-				.setPlaceholder("A short(ish) description.")
-				.setMinLength(1)
-				.setMaxLength(500)
-				.build();
-
-		TextInput tags = TextInput.create("tags", "Player Requirements", TextInputStyle.PARAGRAPH)
-				.setPlaceholder(
-						"Example:\n- New/Veteran/All Players\n- Serious/Fun/Novelty Campaign\n- RP/Combat Heavy")
-				.setMinLength(1)
-				.setMaxLength(100)
-				.build();
-
-		Modal modal = Modal.create("create-campaign", "Create Campaign")
-				.addComponents(ActionRow.of(name), ActionRow.of(description), ActionRow.of(tags))
-				.build();
-
-		event.replyModal(modal).queue();
-	}
-
 	@Override
 	public void onModalInteraction(ModalInteractionEvent event) {
 		if (event.getModalId().equals("create-campaign")) {
-			if (event.getGuild().getRoles().stream().map(role -> role.getName().toLowerCase()).toList()
-					.contains(event.getValue("name").getAsString().toLowerCase())) {
-				event.reply("Couldn't create campaign. A role likely already exists with that name.")
-						.setEphemeral(true)
-						.queue();
-				return;
-			}
-			event.deferReply().setEphemeral(true).queue();
-			try {
-				String campaignName = event.getValue("name").getAsString();
-				String campaignDescription = event.getValue("description").getAsString();
-				String campaignTags = event.getValue("tags").getAsString();
-				Member DM = event.getMember();
-
-				Guild guild = event.getGuild();
-				Role player = guild.createRole().setName(campaignName).complete();
-				Role gameMaster = guild.createRole().setName(campaignName + " DM").complete();
-				guild.addRoleToMember(DM, gameMaster).queue();
-				Category category = guild.createCategory(campaignName).complete();
-				category.upsertPermissionOverride(guild.getPublicRole()).setDenied(
-						Permission.VIEW_CHANNEL.getRawValue()).complete();
-				category.upsertPermissionOverride(player).setAllowed(
-						Permission.VIEW_CHANNEL).complete();
-				category.upsertPermissionOverride(gameMaster).setAllowed(
-						Permission.VIEW_CHANNEL,
-						Permission.VOICE_DEAF_OTHERS,
-						Permission.VOICE_MUTE_OTHERS,
-						Permission.MANAGE_CHANNEL,
-						Permission.MESSAGE_MANAGE).complete();
-
-				category.createVoiceChannel("Game Room").queue();
-				category.createTextChannel("game-chat").queue();
-
-				TextChannel updateChannel = category.createTextChannel("updates").complete();
-				updateChannel.upsertPermissionOverride(player).setDenied(Permission.MESSAGE_SEND).complete();
-				updateChannel.upsertPermissionOverride(gameMaster).setDenied(Permission.MANAGE_CHANNEL).complete();
-
-				TextChannel controlChannel = category.createTextChannel("DM Screen").complete();
-				controlChannel.upsertPermissionOverride(player).setDenied(Permission.VIEW_CHANNEL).complete();
-				controlChannel.upsertPermissionOverride(gameMaster).setDenied(
-						Permission.MESSAGE_SEND,
-						Permission.MESSAGE_MANAGE,
-						Permission.MANAGE_CHANNEL).complete();
-
-				MessageCreateData forumPost = new MessageCreateBuilder()
-						.addContent(String.format("%s\n\nExpectations:\n%s\n",
-								campaignDescription, campaignTags))
-						.addActionRow(Button.success("join-campaign", "Apply To Join").asDisabled())
-						.build();
-				ForumChannel forum = guild.getForumChannelById(CAMPAIGN_FORUM);
-				ForumPost post = forum.createForumPost(campaignName, forumPost)
-						.setTags(forum.getAvailableTagsByName("PREPARING", false)).complete();
-
-				dbCon.executeSQL(
-						"INSERT INTO campaigns (campaign_name, role_id, category_id, status, post_id) VALUES (?,?,?,?,?)",
-						campaignName, player.getId(), category.getId(), "PREPARING",
-						post.getThreadChannel().getId());
-				event.getHook().sendMessage("Campaign Created Successfully.").setEphemeral(true).queue();
-				guild.getNewsChannelById(GAME_ANNOUNCEMENTS)
-						.sendMessage(new MessageCreateBuilder()
-								.addContent("**NEW CAMPAIGN**").setEmbeds(
-										new EmbedBuilder()
-												.setFooter("By " + event
-														.getUser()
-														.getEffectiveName(),
-														event.getUser().getAvatarUrl())
-												.setTitle(String.format(
-														"**%s**",
-														campaignName))
-												.appendDescription(
-														campaignDescription)
-												.build())
-								.addActionRow(
-										Button.link(post.getMessage()
-												.getJumpUrl(),
-												"Go To Campaign"))
-								.build())
-						.queue();
-				TextChannel textChannel = (TextChannel) category.getChannels().stream()
-						.filter(channel -> channel.getName().equals("dm-screen")).toList().get(0);
-				textChannel.sendMessage(new MessageCreateBuilder().setContent("**DUNGEON MASTER CONTROLS**")
-						.addActionRow(
-								Button.success("open-close", "Open"),
-								Button.primary("schedule", "Schedule Session"),
-								Button.secondary("availability", "Check Availability"),
-								Button.danger("campaign-settings", "Settings"))
-						.build()).queue();
-			} catch (Exception e) {
-				event.getHook().sendMessage("Hmmm. Something went wrong.").setEphemeral(true).queue();
-			}
+			CampaignManagement.createCampaign(event, campaignDAO);
 		} else if (event.getModalId().substring(0, 13).equals("join-request-")) {
 			TextChannel dmScreen = event.getGuild().getCategoryById(event.getModalId().substring(13))
 					.getTextChannels()
