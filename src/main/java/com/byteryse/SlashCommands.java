485fd66693de1eeb1dced446f0b29655aaddaef6
Ben Walker <benjaminjwalker2910@gmail.com>
2024-11-13 14:23:44 +0000

    rename back to slashcommands

--- src/main/java/com/byteryse/SlashCommands.java
+++ src/main/java/com/byteryse/SlashCommands.java
@@ -8,10 +8,10 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class InteractionRouting extends ListenerAdapter {
+public class SlashCommands extends ListenerAdapter {
 	private CampaignDAO campaignDAO;
 
-	public InteractionRouting() {
+	public SlashCommands() {
 		this.campaignDAO = new CampaignDAO();
 	}
 
