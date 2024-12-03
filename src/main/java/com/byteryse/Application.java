1e3dfd104530eca072ba97cae2df84d113ddaa68
Ben Walker <benjaminjwalker2910@gmail.com>
2024-12-03 16:26:48 +0000

    guild member intents enabling and empty resultset stacktrace removal

--- src/main/java/com/byteryse/Application.java
+++ src/main/java/com/byteryse/Application.java
@@ -2,11 +2,17 @@ package com.byteryse;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Application {
 
 	public static void main(String[] args) throws Exception {
-		JDA api = JDABuilder.createDefault(System.getenv("DISCORD_TOKEN")).build();
+		JDA api = JDABuilder
+				.createDefault(System.getenv("DISCORD_TOKEN"))
+				.enableIntents(GatewayIntent.GUILD_MEMBERS)
+				.setMemberCachePolicy(MemberCachePolicy.ALL)
+				.build();
 		api.addEventListener(new InteractionRouting());
 		api.addEventListener(new EventListeners());
 	}
